10124acf13b6a81519266acb1ed3a8e141991a3b
douae-rohand <douaerohand18@gmail.com>
2025-07-15 13:24:55 +0100

    db

--- app/database/connexion.php
+++ app/database/connexion.php
@@ -1,8 +1,18 @@
 <?php
+
+/*
 $host = 'localhost';
 $dbname = 'temasa_db';
 $username = 'root';
 $password = ''; 
+*/
+
+// Lire les variables d'environnement
+$host = getenv('DB_HOST') ?: 'localhost';
+$dbname = getenv('DB_DATABASE') ?: 'temasa_db';
+$username = getenv('DB_USERNAME') ?: 'root';
+$password = getenv('DB_PASSWORD') ?: '';
+
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
@@ -12,3 +22,5 @@ try {
 }
 
 ?>
+
+
